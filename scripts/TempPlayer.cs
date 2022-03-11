24c515f86c6e4d4e27fd26caad34337ae33c6acc
LucasW20 <lucas.wright552@gmail.com>
2022-03-10 21:21:27 -0600

    Better Jump Implementation
    Made better jump controls. Depending on how far into the jump and/or if the player has released the jump button the gravity value changes.

--- scripts/TempPlayer.cs
+++ scripts/TempPlayer.cs
@@ -5,33 +5,33 @@ using System;
  * Handles the first iteration of player movement for Fliers Effect and the time travel
  * @author Lucas_C_Wright
  * @editor Jaden_Patten
+ * @editor Jake_S
  * @start 3-7-2022
  * @version 3-9-2022
  */
 public class TempPlayer : KinematicBody2D {
-	[Export] public float GRAVITY = 750;
-	[Export] public float JUMP = -500;
 	[Export] public int walkSpeed = 400;
 	[Export] public int acc = 1;
+	[Export] public float maxSpeed;
 	private bool TimeTraveled;
 	private Vector2 velocity;
 	private Node2D myNode;
-	[Export] public float maxSpeed;
-	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		walkSpeed = 20;
 		maxSpeed = 400f;
-		TimeTraveled = false;		
+		TimeTraveled = false;
+		jumpHeight = false;
+		jumpRelease = false;
 		velocity = new Vector2();
 		myNode = GetParent().GetNode<Node2D>("TempPlayer");
+		jumpTimer = GetNode<Timer>("JumpTimer");
 	}
 
 	//runs physics checks every frame 
 	public override void _PhysicsProcess(float delta) {
-		//Vertical movement code. Apply gravity.
-		if (!IsOnFloor()) { velocity.y += GRAVITY * delta; }
+		JumpPhysicsProcess(delta);
 
 		//walking input
 		if (Input.IsActionPressed("left")) {
@@ -48,23 +48,6 @@ public class TempPlayer : KinematicBody2D {
 			velocity.x = 0;
 		}
 
-		//jumping input
-		if (Input.IsActionPressed("jump")) {
-			//Console.WriteLine("Pressed Jump");
-			if (IsOnFloor()) {
-				//Console.WriteLine("Jumped");
-				velocity.y = JUMP;
-            }
-        }
-
-		//down input
-		if (Input.IsActionPressed("down")) {
-			if (!IsOnFloor()) { //if the player is in the air then they can press 'S' to shoot down at the cost of some horizontal movement
-				velocity.x -= velocity.x * 0.25f;
-				velocity.y = 500;
-            }
-        }
-
 		//time travel input
 		if (Input.IsActionJustPressed("timetravel")) {
 			//Console.WriteLine("Pressed/n");
@@ -77,8 +60,71 @@ public class TempPlayer : KinematicBody2D {
 			}
 		}
 
-		Console.WriteLine("<" + velocity.x + ", " + velocity.y + ">");
+		////down input REPLACED FOR BETTER JUMP CONTROL
+		//if (Input.IsActionPressed("down")) {
+		//	if (!IsOnFloor()) { //if the player is in the air then they can press 'S' to shoot down at the cost of some horizontal movement
+		//		velocity.x -= velocity.x * 0.25f;
+		//		velocity.y = 500;
+		//  }
+		//}
+
+		//Console.WriteLine("<" + velocity.x + ", " + velocity.y + ">");
+		
 		//actually move the player
 		MoveAndSlide(velocity, new Vector2(0, -1));
 	}
+	
+	//gravity and jump variables
+	private const float NORMAL_GRAVITY = 750;
+	private const  float FAST_FALL_GRAVITY = 1500;
+	private const float JUMP = -500;
+	private bool jumpHeight;
+	private bool jumpRelease;
+	private Timer jumpTimer;
+
+	/* There are two systems for a better jump.
+	* 1. Max Height. When the player reaches the maximum height of the jump (determined by the timer) gravity is
+	* switched to the FAST_FALL_GRAVITY. This is to make jumps feel less floaty. Uses timer and jumpHeight bool 
+	* to determine when to swap the gravity. Reset jumpHeight when the player presses the jump button. 
+	* 2. Release Jump. When the player releases the jump button then the gravity is switched to the FAST_FALL_GRAVITY.
+	* This is to give better jump control to the player. Uses jumpRelease bool to determine when the button is released
+	* and to switch to FAST_FALL_GRAVITY. Reset jumpRelease to false when player presses the jump button. 
+	*/
+	private void JumpPhysicsProcess(float delta) {
+		//Vertical movement code. Apply gravity. Only apply while their in the air.
+		if (!IsOnFloor()) { 
+			if (!jumpHeight || !jumpRelease) { //if they're just falling normally use normal gravity
+				velocity.y += NORMAL_GRAVITY * delta;
+			} else { //if they've jumped after a time or released jump button use faster gravity
+				velocity.y += FAST_FALL_GRAVITY * delta;
+			}
+		}
+
+		//when the player presses the jump button initiate the jump by changing the velocity
+		if (Input.IsActionPressed("jump")) {
+			if (IsOnFloor()) { //cant jump while on the ground
+				velocity.y = JUMP;
+				//setup for jump control
+				jumpHeight = false;
+				jumpRelease = false;
+				jumpTimer.Start();
+			}
+		}
+
+		//when the player releases the jump button swap over to the fast fall gravity
+		if (Input.IsActionJustReleased("jump")) {
+			if (!IsOnFloor()) { 
+				//Console.WriteLine("Jump Input released! Switching to Fast Fall Gravity");
+				jumpRelease = true;
+				velocity.y *= 0.6f;
+			}
+		}
+	}
+
+    //method for when the timer runs out. 
+    private void _on_JumpTimer_timeout() {
+		Console.WriteLine("JumpTimer Timeup! Switching to Fast Fall Gravity");
+        jumpHeight = true;
+        jumpTimer.Stop();
+    }
 }
