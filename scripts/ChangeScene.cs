c5bbb7c2be02cfc2417ce1e0cbad3b0d0d2b1f64
LucasW20 <lucas.wright552@gmail.com>
2022-03-21 15:20:30 -0500

    Added Fade Out to Scene Change

--- scripts/ChangeScene.cs
+++ scripts/ChangeScene.cs
@@ -8,35 +8,42 @@ using System;
  * @version 3-21-2022
  */
 public class ChangeScene : Node {
+	private Sprite fadeImage;
+	private Timer exitTimer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		
+		fadeImage = GetChild<Sprite>(0);
+		exitTimer = GetChild<Timer>(2);
 	}
 
-	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-	//  public override void _Process(float delta)
-	//  {
-	//      
-	//  }
-
-	private void OnBodyEntered(object body) {
-		if (body.GetType().Name.Equals("TempPlayer")) {
-			Console.WriteLine("Entered Area");
-			GetTree().ChangeScene("res://scenes/PrisonLevel.tscn");
+	//runs when the player enteres the area that exits the scene
+    private void OnBodyEntered(object body) {
+		if (body.GetType().Name.Equals("TempPlayer")) { //if the player and not another object enters the exit area
+			//Console.WriteLine("Entered Area");
+			//fade out the scene 
+			FadeOut();
 		}
 	}
 
-	private void OnStartButtonPressed() {
-		GetTree().ChangeScene("res://scenes/MainScene.tscn");
-	}
+	//fades out the scene and then changes the scene
+    async private void FadeOut() {
+        fadeImage.Modulate = new Color(fadeImage.Modulate, 0); //make sure the alpha of the image is 0
 
+		//loop to gradually change the the alpha value by 0.025 each iteration
+        for (float i = 0; i <= 1; i += 0.025f) {
+			//change the alpha
+            fadeImage.Modulate = new Color(fadeImage.Modulate, i);
+            Console.WriteLine(fadeImage.Modulate.a);
+
+			//wait for the timer to finish so the fade doesn't look instant. Timer takes 0.05 seconds to finish
+            exitTimer.Start();
+            await ToSignal(exitTimer, "timeout");
+        }
+
+		//when the fade has completed actually change the scene
+        GetTree().ChangeScene("res://scenes/PrisonLevel.tscn");
+    }
+
+	private void OnExitTimerRunout() { } //do nothing
 }
-
-
-
-
-
-
-
-
