9937c279db3ef5b77de3dbf362cd059a8e64f6e8
Alex Hartz <alexhartz99@gmail.com>
2022-03-24 20:57:12 -0500

    Rough Player Animation (Run)

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -3,36 +3,36 @@ using System;
 
 public class MainMenu : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
 
-    }
+	}
 
-    private void OnStartPressed()
-    {
-        Console.WriteLine("Start Button was pressed");
-        GetTree().ChangeScene("res://scenes/MainScene.tscn");
-    }
+	private void OnStartPressed()
+	{
+		Console.WriteLine("Start Button was pressed");
+		GetTree().ChangeScene("res://scenes/MainScene.tscn");
+	}
 
-    private void OnOptionsPressed()
-    {
-        Console.WriteLine("Options Button was pressed");
-    }
+	private void OnOptionsPressed()
+	{
+		Console.WriteLine("Options Button was pressed");
+	}
 
-    private void OnExitPressed()
-    {
-        Console.WriteLine("Exit Button was pressed");
-        GetTree().Quit();
-    }
+	private void OnExitPressed()
+	{
+		Console.WriteLine("Exit Button was pressed");
+		GetTree().Quit();
+	}
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	//  public override void _Process(float delta)
+	//  {
+	//      
+	//  }
 }
