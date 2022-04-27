b43bb6979432356bdbac0180c67ee38784545ec2
LucasW20 <lucas.wright552@gmail.com>
2022-04-27 14:46:51 -0500

    Loading screen

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -1,32 +1,51 @@
 using Godot;
 using System;
 
+/**
+ * Handles the button interactions for the Main Menu screen
+ * @author Jaden_Patten
+ * @editor Lucas_C_Wright
+ * @version 04-27-2022
+ */
 public class MainMenu : Control
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	private Timer myTimer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		myTimer = GetNode<Timer>("Timer");
 	}
 
 	private void OnStartPressed()
 	{
-		Console.WriteLine("Start Button was pressed");
+		StartGame();
+	}
+
+	async private void StartGame() {
+		GetNode<AudioStreamPlayer>("AudioStreamPlayer").Stop();
+		myTimer.Start();
+		await ToSignal(myTimer, "timeout");
+
+		//SWAP TO LOADING IMG
+		GetNode<Node2D>("Front").Visible = false;
+		myTimer.Start();
+
+		//wait for the timer to finish
+		await ToSignal(myTimer, "timeout");
+
+		//CHANGE SCENE
 		GetTree().ChangeScene("res://scenes/Levels/Past/Level 1 Past.tscn");
 	}
 
 	private void OnOptionsPressed() 
 	{
-		Console.WriteLine("Options Button was pressed");
+		//Console.WriteLine("Options Button was pressed");
 	}
 
 	private void OnExitPressed()
 	{
-		Console.WriteLine("Exit Button was pressed");
+		//Console.WriteLine("Exit Button was pressed");
 		GetTree().Quit();
 	}
 
@@ -35,4 +54,6 @@ public class MainMenu : Control
 	//  {
 	//      
 	//  }
+
+	public void onTimerTimeout() { } //do nothing
 }
